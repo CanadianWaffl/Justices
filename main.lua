da79b139aca00f9ccf67115b301f79b411a50b50
CanadianWaffl <lochlan@elven.ca>
2025-03-11 15:54:51 -0700

    First functional version

---
+++ main.lua
@@ -0,0 +1,118 @@
+SMODS.Atlas{
+    key = 'Justices',
+    path = 'jokeratlas.png',
+    px = 71,
+    py = 95
+}
+
+SMODS.Joker{
+    key = 'neil',
+    loc_txt = {
+        name = 'Neil Gorsuch',
+        text = {
+            'Hi, I\'m Neil'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 0, y = 0}
+}
+
+SMODS.Joker{
+    key = 'john',
+    loc_txt = {
+        name = 'John Roberts',
+        text = {
+            'Hello. I\'m John G. Roberts.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 1, y = 0}
+}
+
+SMODS.Joker{
+    key = 'clarence',
+    loc_txt = {
+        name = 'Clarence Thomas',
+        text = {
+            'I\'m Clarence,',
+            'nice to meet you.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 2, y = 0}
+}
+
+SMODS.Joker{
+    key = 'sam',
+    loc_txt = {
+        name = 'Samuel Alito',
+        text = {
+            '\'Sup, It\'s Sam'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 0, y = 1}
+}
+
+SMODS.Joker{
+    key = 'sonia',
+    loc_txt = {
+        name = 'Sonia Sotomayor',
+        text = {
+            'Sotomayor, pleased to make',
+            'your acquaintance.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 1, y = 1}
+}
+
+SMODS.Joker{
+    key = 'elena',
+    loc_txt = {
+        name = 'Elena Kagan',
+        text = {
+            'Hi, I\'m Mrs. Kagan.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 2, y = 1}
+}
+
+SMODS.Joker{
+    key = 'brett',
+    loc_txt = {
+        name = 'Brett Kavanaugh',
+        text = {
+            'Hola, soy Brett.',
+            'I\'ve been practicing',
+            'my espanol'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 0, y = 2}
+}
+
+SMODS.Joker{
+    key = 'amy',
+    loc_txt = {
+        name = 'Amy Coney Barrett',
+        text = {
+            'Hi, I have a middle name.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 1, y = 2}
+}
+
+SMODS.Joker{
+    key = 'ketanji',
+    loc_txt = {
+        name = 'Ketanji Jackson',
+        text = {
+            'Hi, I\'m Ketanji.'
+        }
+    },
+    atlas = 'Justices',
+    pos = {x = 2, y = 2}
+}
